28b7f2d155bc5ac3552e831c42d42ddeba583f72
Emoncr <biplobemon 75945@gmail.com>
2023-12-16 03:55:56 +0600

    complate about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="py-16 sm:py-120 about_section bg-cover bg-no-repeat bg-center">
+      <div className="container">
+        <div className="content_box max-w-[1096px] mx-auto py-10 sm:py-24 px-4 bg-white">
+          <h2 className="uppercase text-2xl sm:text-45 font-normal font-cinzel text-brandDark text-center">Honey & Salt</h2>
+          <p className="font-inter font-normal text-brandDark max-w-[762px] mx-auto text-center text-base mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 

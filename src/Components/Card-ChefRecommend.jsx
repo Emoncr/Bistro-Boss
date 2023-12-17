07095607a49389ca4249b9872833cfbfc6b41917
Emoncr <biplobemon 75945@gmail.com>
2023-12-17 12:08:43 +0600

    chef recommends section complate

---
+++ src/Components/Card-ChefRecommend.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ChefRecommendCard = () => {
+    return (
+        <div className='max-w-[424px] mx-auto'>
+            <div className="image_container">
+                <img className='lg:min-h-[300px] max-h-[300px] w-full h-full object-cover ' src="/dynamic images/chef recomand Image.png" alt="Card Image" />
+            </div>
+            <div className="card_body bg-[#F3F3F3] px-5 py-6 sm:px-8 sm:py-6 lg:px-10 lg:py-8">
+                <h4 className='text-brandDark text-base sm:text-xl lg:text-[24px] font-inter font-semibold  text-center'>Caeser Salad</h4>
+                <p className='text-sm sm:text-base font-inter font-normal text-center mt-2'>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+
+                <div className="btn-container flex items-center justify-center mt-6">
+                    <button className='brand_btn_yellow'>
+                        add to cart
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChefRecommendCard

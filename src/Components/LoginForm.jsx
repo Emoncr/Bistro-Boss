65ea7b6798dc7fdbcb1c4de5c72efbf7b8266d00
Emoncr <biplobemon 75945@gmail.com>
2023-12-29 23:36:51 +0600

    created sign in form

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const LoginForm = () => {
+    return (
+        <div>
+            <h4 className='font-inter text-brandDark text-3xl md:text-[40px] font-bold text-center'>Login</h4>
+            <form className='mt-5 mx-5'>
+                <label htmlFor="name" className="text-[#444] font-inter text-xl font-semibold flex flex-col">Email*
+                    <input
+                        type="email"
+                        name="name"
+                        required
+                        placeholder="Type here"
+                        className="input_feild mt-2 sm:mt-4"
+                    />
+                </label>
+                <label htmlFor="name" className="text-[#444] font-inter text-xl font-semibold flex flex-col mt-6">Password*
+                    <input
+                        type="email"
+                        name="name"
+                        required
+                        placeholder="Enter your password"
+                        className="input_feild mt-2 sm:mt-4"
+                    />
+                </label>
+
+
+                <button
+                    type='submit'
+                    className='w-full py-4 sm:py-6 rounded-lg bg-brandPrimary/70 mt-5 text-white text-base sm:text-xl font-bold font-inter'
+                >
+                    Sign In
+                </button>
+
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm

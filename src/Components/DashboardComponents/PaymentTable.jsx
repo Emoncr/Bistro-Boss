85dc5a4b5cc042b248f60b096486c8eecdda726a
Emoncr <biplobemon 75945@gmail.com>
2024-01-02 01:34:52 +0600

    payment page complate

--- src/Components/DashboardComponents/PaymentTable.jsx
+++ src/Components/DashboardComponents/PaymentTable.jsx
@@ -3,96 +3,36 @@ import React from 'react'
 const PaymentTable = () => {
     return (
         <>
-            <div className="max-w-full mx-auto">
+            <div className="max-w-full mx-auto overflow-x-auto">
                 <div className="flex flex-col">
                     <div className="overflow-x-auto sm:rounded-t-lg">
                         <div className="inline-block min-w-full align-middle">
-                            <div className="overflow-hidden">
-                                <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                                     <thead className="bg-brandPrimary text-white font-inter font-semibold">
-                                        <tr >
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase dark:text-gray-400"
-                                            >
+                                        <tr>
+                                            <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left uppercase dark:text-gray-400">
                                                 Email
                                             </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase dark:text-gray-400"
-                                            >
+                                            <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left uppercase dark:text-gray-400">
                                                 Category
                                             </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase dark:text-gray-400"
-                                            >
+                                            <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left uppercase dark:text-gray-400">
                                                 Total Price
                                             </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase dark:text-gray-400"
-                                            >
+                                            <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left uppercase dark:text-gray-400">
                                                 Date
                                             </th>
-
                                         </tr>
                                     </thead>
-                                    <tbody className=" divide-y divide-[#E8E8E8]  text-[#737373] ">
-                                        <tr >
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left     dark:text-gray-400"
-                                            >
-                                                biplob@gmail.com
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left    dark:text-gray-400"
-                                            >
-                                                Cart
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase dark:text-gray-400"
-                                            >
-                                               $445
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase "
-                                            >
-                                                Mon,22/4/24
-                                            </th>
-
+                                    <tbody className="divide-y divide-gray-200 dark:divide-[#E8E8E8] text-[#737373]">
+                                        <tr>
+                                            <td className="py-5 px-6 text-xs font-medium tracking-wider text-left dark:text-gray-400">biplob@gmail.com</td>
+                                            <td className="py-5 px-6 text-xs font-medium tracking-wider text-left dark:text-gray-400">Cart</td>
+                                            <td className="py-5 px-6 text-xs font-medium tracking-wider text-left uppercase dark:text-gray-400">$445</td>
+                                            <td className="py-5 px-6 text-xs font-medium tracking-wider text-left uppercase">Mon,22/4/24</td>
                                         </tr>
-                                        <tr >
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left     dark:text-gray-400"
-                                            >
-                                                biplob@gmail.com
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left    dark:text-gray-400"
-                                            >
-                                                Cart
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase dark:text-gray-400"
-                                            >
-                                               $445
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="py-5 px-6 text-xs font-medium tracking-wider text-left   uppercase "
-                                            >
-                                                Mon,22/4/24
-                                            </th>
 
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
@@ -100,6 +40,7 @@ const PaymentTable = () => {
                     </div>
                 </div>
             </div>
+
         </>
     )
 }

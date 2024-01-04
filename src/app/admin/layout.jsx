d5673c96cc9e2e36e0855ad7e04c849c3e8e5181
Emoncr <biplobemon 75945@gmail.com>
2024-01-04 20:22:13 +0600

    deployment fixed

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,27 @@
+import UserDashboardNav from '@/Components/DashboardComponents/UserDashboardNav'
+import React from 'react'
+
+const layout = ({ children }) => {
+    return (
+        <>
+            <section className='min-h-screen h-full'>
+                <div className="grid grid-cols-12 ">
+                    <div className="sidebar_container py-7 md:py-12 bg-brandPrimary col-span-2 sm:col-span-1 lg:col-span-3 xl:col-span-2  min-h-screen h-full ">
+                        <div className="logo_container px-2 sm:px-4 lg:px-8 ">
+                            <img src="/images/logo black.png" alt="Logo" />
+                        </div>
+                        <div className="links_container mt-10 lg:mt-16">
+                            <UserDashboardNav />
+                        </div>
+                    </div>
+                    <div className="col-span-10 sm:col-span-11 lg:col-span-9 xl:col-span-10 ">
+                        {children}
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default layout
